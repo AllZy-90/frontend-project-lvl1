d33e03c1407c4c133aeb0c5bf957504527138e3a
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-07 17:06:29 +0300

    brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/no-unresolved
-import startGame from '../src/games/brain-even-game.js';
+import { startGame } from '../src/games/brain-even-games.js';
 
 startGame();
