38d4e319d478cf0759f440a5be7a3238db6548b6
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-07 17:07:18 +0300

    brain-even-games.js

--- src/games/brain-even-games.js
+++ src/games/brain-even-games.js
@@ -6,14 +6,13 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => (number % 2) === 0;
 
 const generateRound = () => {
-  const randomNumber = getRandom(0, 1000);
+  const randomNumber = getRandom(0, 100);
   const question = randomNumber;
   const result = isEven(randomNumber) ? 'yes' : 'no';
   return [question, result];
 };
 
-const startGame = () => {
+// eslint-disable-next-line import/prefer-default-export
+export const startGame = () => {
   invokeGameFunction(generateRound, gameRules);
 };
-
-export default startGame;
