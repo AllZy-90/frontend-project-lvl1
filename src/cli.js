fe2875b8c84ceabc07708f3faec10fa3e69d0c64
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-07 11:50:58 +0300

    cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
+
+// eslint-disable-next-line import/prefer-default-export
 export const printName = () => {
-   const userName = readlineSync.question('May I have your name? ');
-   console.log ('Hello, ' + userName + '!'); 
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
