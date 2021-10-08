43bafd43f667840537e3cc3c50996e92fab2ced5
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-08 15:33:15 +0300

    new game

---
+++ src/games/brain-prime-game.js
@@ -0,0 +1,28 @@
+import getRandom from '../utils.js';
+import invokeGameFunction from '../index.js';
+
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = getRandom(0, 50);
+  const result = isPrime(question) ? 'yes' : 'no';
+  return [question, result];
+};
+
+const startGame = () => {
+  invokeGameFunction(generateRound, gameRules);
+};
+
+export default startGame;
