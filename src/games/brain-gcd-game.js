ad0609be97181011d5604b587b5a24e3832d0c73
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-08 13:57:46 +0300

    brain-gcd-game

---
+++ src/games/brain-gcd-game.js
@@ -0,0 +1,25 @@
+import getRandom from '../utils.js';
+import invokeGameFunction from '../index.js';
+
+const gameRules = 'Find the greatest common divisor of given numbers';
+
+const findGCD = (number1, number2) => {
+  if (number2 === 0) {
+    return Math.abs(number1);
+  }
+  return findGCD(number2, number1 % number2);
+};
+
+const generateRound = () => {
+  const firstNumber = getRandom(0, 100);
+  const secondNumber = getRandom(0, 100);
+  const question = `${firstNumber} ${secondNumber}`;
+  const result = findGCD(firstNumber, secondNumber);
+  return [question, String(result)];
+};
+
+const startGame = () => {
+  invokeGameFunction(generateRound, gameRules);
+};
+
+export default startGame;
