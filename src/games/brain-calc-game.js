5707f353352691b4da31b581982ad0d339ba3da5
Alla-Zyapaeva <allazyapaeva@gmail.com>
2021-10-08 13:11:47 +0300

    brain-calc-game.js

---
+++ src/games/brain-calc-game.js
@@ -0,0 +1,31 @@
+import getRandom from '../utils.js';
+import invokeGameFunction from '../index.js';
+
+const gameRules = 'What is the result of the expression?';
+
+const calculateExpression = (firstNumber, secondNumber, expressionSign) => {
+  switch (expressionSign) {
+    case '+': return firstNumber + secondNumber;
+    case '-': return firstNumber - secondNumber;
+    case '*': return firstNumber * secondNumber;
+    default:
+      throw new Error('Can`t calculate expression');
+  }
+};
+
+const generateRound = () => {
+  const expressionsSignArray = ['+', '-', '*'];
+  const expressionNumber = getRandom(0, expressionsSignArray.length - 1);
+  const firstNumber = getRandom(0, 100);
+  const secondNumber = getRandom(0, 100);
+  const sign = expressionsSignArray[expressionNumber];
+  const question = `${firstNumber} ${sign} ${secondNumber}`;
+  const result = calculateExpression(firstNumber, secondNumber, sign);
+  return [question, String(result)];
+};
+
+const startGame = () => {
+  invokeGameFunction(generateRound, gameRules);
+};
+
+export default startGame;
